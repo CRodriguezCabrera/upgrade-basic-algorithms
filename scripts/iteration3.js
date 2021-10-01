8a949fe816cddd38c43ba4b4fec5d3bbb87686f4
ZEEKER <zeeker@MacBook-Air-de-ZEEKER.local>
2021-10-01 17:58:06 +0200

    iteration3

---
+++ scripts/iteration3.js
@@ -0,0 +1,18 @@
+// iteration 1.1
+console.log(10*5)
+
+//iteration 1.2
+console.log(10/2)
+
+//iteration 1.3
+console.log (15/9)
+
+/* //iteration 1.4
+let y = 10;
+let z = 5;
+console.log(x = y+z) */
+
+//iteration 1.5
+let y = 10;
+let z = 5;
+console.log(x=y*z)
