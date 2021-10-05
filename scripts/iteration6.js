dcc7a78755095ada1f40efa341b662bae52e16c5
ZEEKER <zeeker@MacBook-Air-de-ZEEKER.local>
2021-10-05 16:14:28 +0200

    iteration6

---
+++ scripts/iteration6.js
@@ -0,0 +1,22 @@
+/* // iteración 1.1
+for (let index = 0; index <= 9; index++) {
+    console.log(index);    
+} */
+
+/* //iteration 1.2
+for (let index = 0; index <= 9; index++){
+    if(index % 2 == 0){
+        console.log(index)
+    }
+} */
+
+//iteration 1.3
+for (let index = 0; index <= 10; index++){
+    if(index <= 9){
+        console.log("intentando dormir")
+    }else {
+        console.log("dormido!")
+    }
+}
+
+
